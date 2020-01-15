57e34db7193befa21b6281f1d276e63e723be659
diegoquezadas <57364472+diegoquezadas@users.noreply.github.com>
2020-01-15 00:03:05 -0500

    a

---
+++ servicio1-singer/src/main/java/com/distribuida/servicio/SingerServiceImpl.java
@@ -0,0 +1,30 @@
+package com.distribuida.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import com.distribuida.dto.Singer;
+
+@ApplicationScoped
+public class SingerServiceImpl implements SingerService{
+	
+	@Inject private EntityManager em;
+
+	@Override
+	public Singer buscarPorId(Integer id) {
+		List<Singer> singers;
+		singers = em.createQuery("SELECT u FROM Singer u WHERE id = :id", Singer.class).setParameter("id", id).getResultList();
+		return singers.get(0);
+	}
+
+	@Override
+	public List<Singer> listarTodos() {
+		List<Singer> singers = new ArrayList<>();
+		singers.add(new Singer());
+//		singers = em.createQuery("SELECT u FROM Singer u", Singer.class).getResultList();
+		return singers;
+	}
+}
